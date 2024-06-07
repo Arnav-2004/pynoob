f0511ddceb8408e2c7ffbff7b6c85e02cae25734
Arnav-2004 <arnavrangwani@gmail.com>
2024-06-07 17:04:16 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# PyNoob
+
+Your go-to resource for mastering Python. Clear explanations, and practical examples to guide your journey.
+
+## License
+
+Licensed under the MIT License, Copyright © 2020-present Greg Bergé.
+
+See [LICENSE](./LICENSE) for more information.
---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "smooth-doc",
+      options: {
+        name: "PyNoob",
+        description: "Your go-to resource for mastering Python. Clear explanations, and practical examples to guide your journey.",
+        siteUrl: "https://pynoob.vercel.app",
+        githubRepositoryURL: "https://github.com/Arnav-2004",
+        navItems: [{ title: "Docs", url: "/docs/" }],
+        sections: ["Docs", "Quick Docs"],
+      },
+    },
+  ],
+};
